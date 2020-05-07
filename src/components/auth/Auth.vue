2bb74a5ac630cfb1b7e9222e9cc0e14e55618f66
Vladimir Borutkin <v.borutkin@gmail.com>
2020-05-07 18:47:31 +0700

    fix auth validations

--- src/components/auth/Auth.vue
+++ src/components/auth/Auth.vue
@@ -8,12 +8,14 @@
         <input id="Username" class="form-control"
                :class="[{'is-invalid': $v.username.$error}, {'is-valid': !$v.username.$error}]"
                placeholder="Username" autofocus
-               @blur="setUsername($event.target.value)">
+               @input="setUsername($event.target.value)">
         <div class="invalid-feedback" v-if="!$v.username.minLength || !$v.username.maxLength">
           Имя пользователя должно содержать минимум {{$v.username.$params.minLength.min}}
           знака и максимум {{$v.username.$params.maxLength.max}}
         </div>
-
+        <div class="invalid-feedback" v-if="!$v.username.required">
+          Обязательное поле
+        </div>
       </div>
       <div>
         <label for="inputPassword" class="sr-only">Password</label>
@@ -22,10 +24,13 @@
                class="form-control mt-2"
                :class="[{'is-invalid': $v.password.$error}, {'is-valid': !$v.password.$error}]"
                placeholder="Password"
-               @blur="setPassword($event.target.value)">
+               @input="setPassword($event.target.value)">
         <div class="invalid-feedback" v-if="!$v.password.minLength || !$v.password.maxLength">
           Пароль должен содержать минимум {{$v.password.$params.minLength.min}}
           знака и максимум {{$v.password.$params.maxLength.max}}
+        </div>
+        <div class="invalid-feedback" v-if="!$v.password.required">
+          Обязательное поле
         </div>
       </div>
       <button class="btn btn-lg btn-primary btn-block mt-2" type="submit" @click.prevent="login">Sign in</button>
@@ -66,13 +71,13 @@ export default {
     ]),
     setPassword (value) {
       this.password = value
-      this.$v.password.$touch()
     },
     setUsername (value) {
       this.username = value
-      this.$v.username.$touch()
     },
     login () {
+      this.$v.password.$touch()
+      this.$v.username.$touch()
       if (!this.$v.$error) {
         this.AUTH({
           username: this.username,
