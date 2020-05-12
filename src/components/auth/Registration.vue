7713652b9b87ea8dc2406176e125b34444cdec86
Vladimir <Borutkin>
2020-05-12 18:56:10 +0700

    validate registration form

--- src/components/auth/Registration.vue
+++ src/components/auth/Registration.vue
@@ -1,44 +1,81 @@
 <template>
-  <b-container class="col-8">
-    <form class="form-signin">
-      <img class="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+  <b-container style="display: flex;justify-content: center; align-content: center;">
+    <form class="form-signin col-6">
       <h1 class="h3 mb-3 font-weight-normal">Регистрация</h1>
-      <div class="d-inline-flex col-12" ref="username">
-        <label for="username" class="col-3">Ваше имя</label>
+
+      <div>
+        <label for="username">Ваше имя</label>
         <input type="text" id="username"
+               ref="username"
                class="form-control"
+               :class="{'is-invalid': $v.username.$error}"
                placeholder="Username"
-               v-model="username"
-        >
+               v-model="username">
+        <div class="invalid-feedback" v-if="!$v.username.minLength || !$v.username.maxLength">
+          Имя пользователя должно содержать минимум {{$v.username.$params.minLength.min}}
+          знака и максимум {{$v.username.$params.maxLength.max}}
+        </div>
+        <div class="invalid-feedback" v-if="!$v.username.required">
+          Обязательное поле
+        </div>
       </div>
-      <div class="d-inline-flex col-12 mt-2" ref="email">
-        <label for="inputEmail" class="col-3">Электронная почта</label>
+      <div class="mt-2" >
+        <label for="inputEmail">Электронная почта</label>
         <input type="text"
                id="inputEmail"
                class="form-control"
+               ref="email"
+               :class="{'is-invalid': $v.email.$error}"
                placeholder="Email address"
-               v-model="email"
-        >
+               v-model="email">
+        <div class="invalid-feedback" v-if="!$v.email.required">
+          Обязательное поле
+        </div>
+        <div class="invalid-feedback" v-if="!$v.email.email">
+          Введите корректный электронный адрес
+        </div>
       </div>
-      <div class="d-inline-flex col-12 mt-2">
-        <label for="inputPassword" class="col-3">Пароль</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" v-model="password">
+      <div class="mt-2" ref="password1">
+        <label for="inputPassword">Пароль</label>
+        <input type="password"
+               id="inputPassword"
+               class="form-control"
+               placeholder="Password"
+               :class="{'is-invalid': $v.password.$error}"
+               v-model="password">
+        <div class="invalid-feedback" v-if="!$v.password.minLength || !$v.password.maxLength">
+        Пароль должен содержать минимум {{$v.password.$params.minLength.min}}
+        знака и максимум {{$v.password.$params.maxLength.max}}
       </div>
-      <div class="d-inline-flex col-12 mt-2">
-        <label for="inputPasswordConfirm" class="col-3">Подтвердите пароль</label>
-        <input type="password" id="inputPasswordConfirm" class="form-control" placeholder="Password" v-model="passwordConfirm">
+        <div class="invalid-feedback" v-if="!$v.password.required">
+          Обязательное поле
+        </div>
+      </div>
+      <div class="mt-2" ref="password2">
+        <label for="inputPasswordConfirm">Подтвердите пароль</label>
+        <input type="password"
+               id="inputPasswordConfirm"
+               class="form-control"
+               placeholder="Password"
+               :class="{'is-invalid': $v.passwordConfirm.$error}"
+               v-model="passwordConfirm">
+
+        <div class="invalid-feedback" v-if="!$v.passwordConfirm.required">
+          Обязательное поле
+        </div>
+        <div class="invalid-feedback" v-if="!$v.passwordConfirm.sameAs">
+          Пароли не совпадают
+        </div>
       </div>
       <button class="btn btn-lg btn-primary mt-2" type="submit" @click.prevent="registration">Регистрация</button>
       <p class="mt-5 mb-3 text-muted">2020</p>
     </form>
-    <div v-if="errors">
-      <p style="color: red" v-for="(error, key) in errors" :key="key">{{error[0]}}</p>
-    </div>
   </b-container>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
+import { minLength, maxLength, required, email, sameAs } from 'vuelidate/lib/validators'
+import { mapActions } from 'vuex'
 export default {
   name: 'Registration',
   data () {
@@ -46,35 +83,60 @@ export default {
       username: '',
       email: '',
       password: '',
-      passwordConfirm: '',
-      errors: []
+      passwordConfirm: ''
     }
   },
-  computed: {
-    ...mapGetters([
-      'IS_REGISTRATION'
-    ])
+  validations: {
+    email: {
+      email,
+      required
+    },
+    username: {
+      required,
+      minLength: minLength(4),
+      maxLength: maxLength(16)
+    },
+    password: {
+      required,
+      minLength: minLength(8),
+      maxLength: maxLength(16)
+    },
+    passwordConfirm: {
+      required,
+      sameAs: sameAs('password')
+    }
   },
   methods: {
     ...mapActions([
       'REGISTRATION'
     ]),
     registration () {
-      this.REGISTRATION({
-        username: this.username,
-        email: this.email,
-        password1: this.password,
-        password2: this.passwordConfirm
-      }).then(response => {
-        if (response.status === 201) {
-          alert('Вы успешно зарегистрировались, вам придет письмо с дальнейшими инструкциями')
-        }
-      }).catch(error => { // TODO зафиксить, перенести в vuex
-        for (const item in error.response.data) {
-          this.$refs[item].insertAdjacentHTML('afterend',
-            `<p style="color: red">${error.response.data[item][0]}</p>`)
-        }
-      })
+      this.$v.password.$touch()
+      this.$v.email.$touch()
+      this.$v.passwordConfirm.$touch()
+      this.$v.username.$touch()
+      if (document.querySelectorAll('.error-api')) {
+        document.querySelectorAll('.error-api').forEach(e => e.parentNode.removeChild(e))
+      }
+
+      if (!this.$v.$error) {
+        this.REGISTRATION({
+          username: this.username,
+          email: this.email,
+          password1: this.password,
+          password2: this.passwordConfirm
+        }).then(response => {
+          if (response.status === 201) {
+            alert('Вы успешно зарегистрировались, вам придет письмо с дальнейшими инструкциями')
+          }
+        }).catch(error => { // TODO зафиксить, перенести в vuex
+          for (const item in error.response.data) {
+            this.$refs[item].classList.add('is-invalid')
+            this.$refs[item].insertAdjacentHTML('afterEnd',
+              `<p style="color: #dc3545; font-size: 80%; margin-top: .25rem;" class="error-api">${error.response.data[item][0]}</p>`)
+          }
+        })
+      }
     }
   }
 }
